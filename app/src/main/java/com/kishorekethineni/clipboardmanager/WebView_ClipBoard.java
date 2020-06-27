e1d3f8f4740fa98b0219a648d209e795731c73aa
kishorekethneni <kishorec111@gmail.com>
2020-06-27 21:45:59 +0530

    3rd commit Internet permission in manifest - kishore

--- app/src/main/java/com/kishorekethineni/clipboardmanager/WebView_ClipBoard.java
+++ app/src/main/java/com/kishorekethineni/clipboardmanager/WebView_ClipBoard.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebView_ClipBoard extends AppCompatActivity {
 
@@ -15,6 +17,17 @@ public class WebView_ClipBoard extends AppCompatActivity {
         WebView browser = (WebView) findViewById(R.id.webview);
         Intent i=getIntent();
         String Copied_URL=i.getStringExtra("URL");
+        browser.setWebViewClient(new MyBrowser());
+        browser.getSettings().setLoadsImagesAutomatically(true);
+        browser.getSettings().setJavaScriptEnabled(true);
+        browser.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
         browser.loadUrl(Copied_URL);
+    }
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
